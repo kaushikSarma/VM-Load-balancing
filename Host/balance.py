adaa331ba85b8798e7abed4d849531714610f835
rewanth1997 <rewanth1997@gmail.com>
2017-10-08 21:43:21 +0530

    Updates balance.py to work on server side.

--- Host/balance.py
+++ Host/balance.py
@@ -1,6 +1,15 @@
 import requests
+from bottle import run, post, request, response, get, route
 
 prev = -1
+serverCount = 4
+
+def generateServerAddress(base, start):
+    return [base % str(i) for i in range(start, start + serverCount)]
+
+base = "http://192.168.146.%s:8080/stats"
+start = 193
+vmList = generateServerAddress(base, start)
 
 def choose(servers):
     ''' Currently implements round robin '''
@@ -10,7 +19,10 @@ def choose(servers):
 
     return servers[prev]
 
+@route('/',method = 'GET')
 def landing():
-    servers = ['http://localhost:8081', 'http://localhost:8082', 'http://localhost:8083', 'http://localhost:8084']
-    chosen = choose(servers)
+    #servers = ['http://localhost:8081', 'http://localhost:8082', 'http://localhost:8083', 'http://localhost:8084']
+    chosen = choose(vmList)
     return requests.get(chosen)
+
+run(host='0.0.0.0', port=8071, debug=True)
