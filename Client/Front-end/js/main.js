3a25ca2933d048758309612684c5749f30e8b8fe
Kaushik Sarma <kausyap10@gmail.com>
2017-10-08 18:10:44 +0530

    Menu buttons, memory graphs implementation, Function getY() added

--- Client/Front-end/js/main.js
+++ Client/Front-end/js/main.js
@@ -1,27 +1,36 @@
 (function($){
-    var GLOBALS = {
+    var GLOBALS = {                             //  Global variables 
         nservers: 0,
-        updateInterval: 1000,
+        updateInterval: 300,
         dataLength: 20,
-        servers: {}
+        servers: {},
+        currentID: 'cpu',
+        currentCategory: null
     },
-    CPUGraphs = {
+    CPUGraphs = {                               //  Wrapper object for CPU graphs, rendered in CanvasJS
         container: null,
         charts: [],        
         dps: [],
-        xVal: 0
+        xVal: 0,
+        getY: function(chartid){
+            return GLOBALS.servers[chartid].cpu;
+        }
     },
-    MEMGraphs = {
+    MEMGraphs = {                               //  Wrapper object for Memory graphs, rendered in CanvasJS
         container: null,
         charts: [],
         dps: [],
-        xVal: 0
+        xVal: 0,
+        getY: function(chartid){
+            return GLOBALS.servers[chartid].mem[2];            
+        }
     },
-    ServerRequest = {
-        request: function(id){
+    ServerRequest = {                           //  Wrapper for functions to send requests to query.py 
+        request: function(id){                  //  Function to ajax request the stats from a server 
             data = $.ajax({
                 request: 'GET',
                 url: 'http://localhost:8070/stats?id='+id,
+                // url: 'http://192.168.2.107:8070/stats?id='+id,
                 async: false,
                 success: function(status){
                 },
@@ -32,7 +41,7 @@
             data = JSON.parse(data);
             return data;
         },
-        requestNumServers: function(){
+        requestNumServers: function(){          //  Function to ajax request the number of servers running 
             data = $.ajax({
                 request: 'GET',
                 url: 'http://localhost:8070/numservers',
@@ -47,7 +56,7 @@
             return data;
         }
     },
-    PopulateCharts = function(category, type){
+    PopulateCharts = function(category, type){  //  Function to append corresponding charts
         category.container = $('#' + type + 'ChartContainer');
         for(i=1; i <= GLOBALS.nservers; i++){
             category.container.append('<div class="chart" id="' + type + 'Chart' + i + '"></div>');
@@ -60,16 +69,21 @@
                         includeZero: false
                     },      
                     data: [{
-                        type: "area",
+                        type: "splineArea",
                         dataPoints: category.dps[i],
+                        lineThickness: 0,
+                        tickLength: 0,
                         valueFormatString: ' '
                     }]
                 });
         }
     },
-    UpdateCharts = function(category, chartid, y){
-        console.log(chartid, y);
-        yVal = y;
+    ReInitialise = function(){
+        $('.chartContainer').hide();
+        $('#' + GLOBALS.currentID + '').show();
+    },
+    UpdateCharts = function(category, chartid){  //  Function to push data points to charts and render the graph
+        yVal = category.getY(chartid);
         category.dps[chartid].push({
             x: category.xVal,
             y: yVal
@@ -83,13 +97,31 @@
     };
     $(document).ready(function(){
         GLOBALS.nservers = ServerRequest.requestNumServers();
-        PopulateCharts(CPUGraphs, 'cpu');
-        PopulateCharts(MEMGraphs, 'mem');
+        // GLOBALS.nservers = 1;
+        GLOBALS.currentCategory = CPUGraphs;
+        PopulateCharts(CPUGraphs, 'cpu');        
+        PopulateCharts(MEMGraphs, 'mem');        
+        
+        var graphButtons = $('.changeGraph')
+            .bind('click', function(){
+                GLOBALS.currentID = $(this).attr('id');
+                if(GLOBALS.currentID == 'cpu')  {
+                    GLOBALS.currentCategory = CPUGraphs;
+                    $('#memChartContainer').hide();
+                    $('#cpuChartContainer').show();
+                }
+                else if(GLOBALS.currentID == 'mem')  {
+                    GLOBALS.currentCategory = MEMGraphs;
+                    $('#memChartContainer').show();
+                    $('#cpuChartContainer').hide();
+                }
+            });
+
+        //  send stat requests every 1000ms or 1s
         setInterval(function(){
             GLOBALS.servers = ServerRequest.request(0);
             for (var key in GLOBALS.servers) {
-                UpdateCharts(CPUGraphs, key, GLOBALS.servers[key].cpu);
-                UpdateCharts(MEMGraphs, key, GLOBALS.servers[key].cpu);
+                UpdateCharts(GLOBALS.currentCategory, key);
             }
         }, GLOBALS.updateInterval);    
     });
