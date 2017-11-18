8ff4b3bc3c26e0471bab03d36c00f1aa18d5ab96
rewanth1997 <rewanth1997@gmail.com>
2017-11-18 11:08:40 +0530

    Param added for number of requests processed.

--- Monitor/2.py
+++ Monitor/2.py
@@ -1,15 +1,20 @@
 import psutil
 from bottle import run, route
 
+# Static variable - stores number of requests processed
+count = 0
+
 @route('/stats',method = 'GET')
 def process():
     netUtilData = psutil.net_io_counters(pernic=True)
     cpuUtilData = psutil.cpu_percent()
     memUtilData = psutil.virtual_memory()
+    count += 1
     return {
         'net': netUtilData,
         'cpu': cpuUtilData,
-        'mem': memUtilData
+        'mem': memUtilData,
+        'count': count
     }
 
 run(host='localhost', port=8082, debug=True)
